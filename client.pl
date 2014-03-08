03826bc73475b180b7b4c92bc7dc19c739351361
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2014-03-08 13:30:35 +0100

    remove a devcomment

--- client.pl
+++ client.pl
@@ -102,7 +102,6 @@ sub writeToDb {
     if($status eq 'valid') {$status = 0}
     else {$status = 1}
 
-    #print "PageID: $page_id\nRevID: $rev_id\nStatus: $status\nDesc: $desc\n====================\n";
     my $dbh = DBI->connect('dbi:'.$config->{dbi_connection},$config->{dbi_user},$config->{dbi_pw})
         or die "Connection Error: $DBI::errstr\n";
     my $sql = 'insert into ofbi_parse_errors values('.$dbh->quote($page_id).', '.$dbh->quote($rev_id).', '.$dbh->quote($status).', '.$dbh->quote($desc).');';
