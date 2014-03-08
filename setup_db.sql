a5067265d37fc720f1101f110f73a971d02b7f52
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2014-03-08 13:08:56 +0100

    Add MySQL setup statements.

--- setup_db.sql
+++ setup_db.sql
@@ -1,5 +1,16 @@
-create table ofbi_parse_errors (pageid INT, revid INT, error_occurred BOOL, error_string VARCHAR);
-create table ofbi_verse (chapterid INT, pageid INT, revid INT, version INT, from_number INT, to_number INT, status INT, text VARCHAR);
-create table ofbi_chapter (bookid INT, number INT);
-create table ofbi_book (osis_name VARCHAR, name VARCHAR);
+SQLite
+======
+
+    create table ofbi_parse_errors (pageid INT, revid INT, error_occurred BOOL, error_string VARCHAR);
+    create table ofbi_verse (chapterid INT, pageid INT, revid INT, version INT, from_number INT, to_number INT, status INT, text VARCHAR);
+    create table ofbi_chapter (bookid INT, number INT);
+    create table ofbi_book (osis_name VARCHAR, name VARCHAR);
+
+MySQL/MariaDB
+=============
+
+    create table ofbi_parse_errors (pageid INT, revid INT, error_occurred BOOL, error_string MEDIUMTEXT);
+    create table ofbi_verse (chapterid INT, pageid INT, revid INT, version INT, from_number INT, to_number INT, status INT, text MEDIUMTEXT);
+    create table ofbi_chapter (bookid INT, number INT);
+    create table ofbi_book (osis_name TINYTEXT, name TINYTEXT);
 
